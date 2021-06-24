a5a9aadba30b96b2c959dd353299cbf72ccf539a
sya-ri <sya79lua@gmail.com>
2021-06-24 22:50:43 +0900

    feat: add AgreeMinecraftEULATask

--- plugin-build/plugin/src/main/java/dev/s7a/gradle/minecraft/server/plugin/MinecraftServerPlugin.kt
+++ plugin-build/plugin/src/main/java/dev/s7a/gradle/minecraft/server/plugin/MinecraftServerPlugin.kt
@@ -3,18 +3,20 @@ package dev.s7a.gradle.minecraft.server.plugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-const val EXTENSION_NAME = "minecraftServerConfig"
-const val TASK_NAME = "launchMinecraftServer"
-
 @Suppress("unused")
 abstract class MinecraftServerPlugin : Plugin<Project> {
     override fun apply(project: Project) {
-        val extension = project.extensions.create(EXTENSION_NAME, MinecraftServerConfig::class.java, project)
+        val extension = project.extensions.create("minecraftServerConfig", MinecraftServerConfig::class.java, project)
 
-        project.tasks.register(TASK_NAME, LaunchMinecraftServerTask::class.java) {
-            it.jarUrl.set(extension.jarUrl)
-            it.jarName.set(extension.jarName)
-            it.serverDirectory.set(extension.serverDirectory)
+        project.tasks.run {
+            register("launchMinecraftServer", LaunchMinecraftServerTask::class.java) {
+                it.jarUrl.set(extension.jarUrl)
+                it.jarName.set(extension.jarName)
+                it.serverDirectory.set(extension.serverDirectory)
+            }
+            register("agreeMinecraftEULA", AgreeMinecraftEULATask::class.java) {
+                it.serverDirectory.set(extension.serverDirectory)
+            }
         }
     }
 }
