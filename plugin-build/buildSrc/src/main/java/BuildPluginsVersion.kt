6b970f1846c7b46925bc5878f79e50aa9c36aff2
sya-ri <sya79lua@gmail.com>
2021-06-27 15:39:55 +0900

    chore: Bump library version - kotlin 1.5.10 → 1.5.20

--- plugin-build/buildSrc/src/main/java/BuildPluginsVersion.kt
+++ plugin-build/buildSrc/src/main/java/BuildPluginsVersion.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
     const val DETEKT = "1.17.1"
-    const val KOTLIN = "1.5.10"
+    const val KOTLIN = "1.5.20"
     const val KTLINT = "10.1.0"
     const val PLUGIN_PUBLISH = "0.14.0"
     const val VERSIONS_PLUGIN = "0.39.0"
