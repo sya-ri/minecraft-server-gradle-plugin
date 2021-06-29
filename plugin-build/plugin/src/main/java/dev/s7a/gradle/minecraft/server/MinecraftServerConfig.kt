f0845126780c8fd5a11c2a50768a4fec5f7d7ade
sya-ri <sya79lua@gmail.com>
2021-06-29 11:03:28 +0900

    refactor: add MinecraftServerConfig::Default

--- plugin-build/plugin/src/main/java/dev/s7a/gradle/minecraft/server/MinecraftServerConfig.kt
+++ plugin-build/plugin/src/main/java/dev/s7a/gradle/minecraft/server/MinecraftServerConfig.kt
@@ -8,6 +8,14 @@ import javax.inject.Inject
 
 @Suppress("UnnecessaryAbstractClass")
 abstract class MinecraftServerConfig @Inject constructor(project: Project) {
+    object Default {
+        const val jarName = "server.jar"
+        fun serverDirectory(project: Project) = project.layout.buildDirectory.dir("MinecraftServer")
+        val jvmArgument = listOf<String>()
+        val serverArgument = listOf<String>()
+        const val nogui = true
+    }
+
     private val objects = project.objects
 
     /**
@@ -18,27 +26,25 @@ abstract class MinecraftServerConfig @Inject constructor(project: Project) {
     /**
      * Server Jar Name
      */
-    val jarName: Property<String> = objects.property(String::class.java).convention("server.jar")
+    val jarName: Property<String> = objects.property(String::class.java).convention(Default.jarName)
 
     /**
      * Server Directory
      */
-    val serverDirectory: DirectoryProperty = objects.directoryProperty().convention(
-        project.layout.buildDirectory.dir("MinecraftServer")
-    )
+    val serverDirectory: DirectoryProperty = objects.directoryProperty().convention(Default.serverDirectory(project))
 
     /**
      * Java Arguments Before .jar
      */
-    val jvmArgument: ListProperty<String> = objects.listProperty(String::class.java).convention(listOf())
+    val jvmArgument: ListProperty<String> = objects.listProperty(String::class.java).convention(Default.jvmArgument)
 
     /**
      * Java Arguments After .jar
      */
-    val serverArgument: ListProperty<String> = objects.listProperty(String::class.java).convention(listOf())
+    val serverArgument: ListProperty<String> = objects.listProperty(String::class.java).convention(Default.serverArgument)
 
     /**
      * Without Console GUI
      */
-    val nogui: Property<Boolean> = objects.property(Boolean::class.java).convention(true)
+    val nogui: Property<Boolean> = objects.property(Boolean::class.java).convention(Default.nogui)
 }
