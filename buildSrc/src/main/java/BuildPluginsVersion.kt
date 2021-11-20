0f6b6f5f169b8452ad58004aa39ee1db20e23821
sya-ri <sya79lua@gmail.com>
2021-11-21 03:48:02 +0900

    chore: bump versions

--- buildSrc/src/main/java/BuildPluginsVersion.kt
+++ buildSrc/src/main/java/BuildPluginsVersion.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
-    const val DETEKT = "1.17.1"
-    const val KOTLIN = "1.5.20"
-    const val KTLINT = "10.1.0"
+    const val DETEKT = "1.18.1"
+    const val KOTLIN = "1.6.0"
+    const val KTLINT = "10.2.0"
     const val VERSIONS_PLUGIN = "0.39.0"
 }
