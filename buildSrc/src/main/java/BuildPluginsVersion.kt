91a752ca1d1f615aac2292154f6bcc023c963fbe
sya-ri <git@s7a.dev>
2022-10-03 11:16:09 +0900

    chore: Bump dependencies

--- buildSrc/src/main/java/BuildPluginsVersion.kt
+++ buildSrc/src/main/java/BuildPluginsVersion.kt
@@ -1,6 +1,6 @@
 object BuildPluginsVersion {
-    const val DETEKT = "1.20.0"
-    const val KOTLIN = "1.6.21"
-    const val KTLINT = "10.3.0"
+    const val DETEKT = "1.21.0"
+    const val KOTLIN = "1.7.20"
+    const val KTLINT = "11.0.0"
     const val VERSIONS_PLUGIN = "0.42.0"
 }
--- plugin-build/buildSrc/src/main/java/BuildPluginsVersion.kt
+++ plugin-build/buildSrc/src/main/java/BuildPluginsVersion.kt
@@ -1,7 +1,7 @@
 object BuildPluginsVersion {
-    const val DETEKT = "1.20.0"
-    const val KOTLIN = "1.6.21"
-    const val KTLINT = "10.3.0"
+    const val DETEKT = "1.21.0"
+    const val KOTLIN = "1.7.20"
+    const val KTLINT = "11.0.0"
     const val PLUGIN_PUBLISH = "0.14.0"
     const val VERSIONS_PLUGIN = "0.42.0"
 }
